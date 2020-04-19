8a1d9fb6a1439a2f203eeec4386a87aaa0695855
Abishek Chandrasekhar <abishekchandrasekhar7@gmail.com>
2020-04-19 18:48:43 +0530

    MWE_with_error

--- MWE_python.py
+++ MWE_python.py
@@ -1,9 +1,31 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Apr 19 18:14:38 2020
+Created on Sun Apr 19 18:37:31 2020
 
 @author: abishek
 """
+from casadi import *
+import numpy as np
+import pandas as pd
+from scipy.integrate import odeint
+import matplotlib.pyplot as plot 
+
+
+
+sim_time = 150
+
+T = 5 #sampling time (min)
+N = 150 #prediction horizon (min)
+control_length = N/T
+control_length = round(control_length)
+
+u_max = 2.500 #Maximum limit on inuput
+u_min = 0 #Maximum limit on inuput
+
+
+
+## Disturbance 
+Ra = np.ones(sim_time)
 
 
 # # States of the system
@@ -13,18 +35,18 @@ x3 = SX.sym('x3')
 x4 = SX.sym('x4')
 x5 = SX.sym('x5')
 states = vertcat(x1, x2, x3, x4, x5)
-n_states = 5 # only gluco-regulatory model and subcutaneous insulin model (without interstitial glucose)
+n_states = 5 
 
 ## Control input
 u = SX.sym('u')
 controls = vertcat(u)
-n_controls = 1 # only insulin is the input
- 
-## Meal disturbance 
-Ra = SX.sym('Ra'); #Rate of appearance of glucose from meal model 
+n_controls = 1 
+
+## disturbance
+Ra = SX.sym('Ra');  
 
 ## Glucose and insulin dynamics
-xdot = #System dynamics
+xdot = vertcat(-0.01*(x1-100)-(x1*x2)+Ra, -x2,-0.05*x3+(0.01*x4+0.005*x5),-0.01*x4+0.01*x5,-(0.01)*x5+u)
 
 f = Function('f',[states,controls,Ra],[xdot]) ## Nonlinear function f(x,u)
 U = SX.sym('U',n_controls,N) ## Decision variables
@@ -41,7 +63,7 @@ for k in range(N):
     state = X[:,k]
     control = U[:,k]
     D = P[n_states + n_states + k]
-    func_value = func(state,control,D)
+    func_value = f(state,control,D)
     # Using Euler integration for propogating the model
     X[:,k+1] = state + (T*func_value)
 
@@ -49,8 +71,8 @@ for k in range(N):
 Q = np.identity(1,dtype = float)
 R = np.identity(1,dtype = float)    
     
-g_ref = 110 # output reference value (glucose in mg/dL)
-u_ref = (0.2*BW)/(24*60) # output reference value (insulin reference in Units/min)
+g_ref = 110 # output reference value 
+u_ref = 0.5 # output reference value (insulin reference in Units/min)
 
 ## Computing cost function 
 J = 0 # initial value of objective function 
@@ -75,8 +97,8 @@ for j in range(N+1):
 Opt_variables = U.T #transpose
 
 ## Nonlinear programming problem formulation
-# nlp_problem = {'func':J,'x':Opt_variables}
+
 nlp_problem = {'f':J,'x':Opt_variables,'g':g,'p':P}
 
 
-solver = nlpsol('solver', 'ipopt', nlp_problem)
+solver = nlpsol('solver', 'sqpmethod', nlp_problem)
